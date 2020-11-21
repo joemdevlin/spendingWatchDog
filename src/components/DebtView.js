844d324d1df713fa973c42a76e2f852e8b7d46cf
Tristan Tam <tristantam@gmail.com>
2020-11-21 00:17:22 -0800

    Finished date function on Debt page

--- src/components/DebtView.js
+++ src/components/DebtView.js
@@ -1,20 +1,7 @@
 import React, { Component, useState } from 'react';
 import DatePicker from "react-datepicker";
-import APIRequester from '../debtApiRequester';
-import { ReactSearchAutocomplete } from "react-search-autocomplete";
-import {Form, Row, Col} from "react-bootstrap";
+import "react-datepicker/dist/react-datepicker.css";
 import { BarChart, Bar, Tooltip, Legend, CartesianGrid, XAxis, YAxis } from 'recharts';
-import { render } from '@testing-library/react';
-
-// https://stackoverflow.com/questions/3895478/does-javascript-have-a-method-like-range-to-generate-a-range-within-the-supp
-const startYears = [...Array(13).keys()].map((i) => {return {id: i, name: (i + 1997).toString()}});
-
-const Example = () => {
-    const [startDate, setStartDate] = useState(new Date());
-    return (
-      <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
-    );
-};
 
 //temp graph input
 const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
@@ -23,68 +10,26 @@ class DebtView extends Component {
     constructor(props){
         super(props);
         this.state = {
-            startDate: new Date(),
-            endDate: new Date()
+            startDate: new Date()
         };
-        /*this.onMonthSearch = this.onMonthSearch.bind(this);
-        this.onYearSearch = this.onYearSearch.bind(this);*/
     }
-    
-      /*
-    this.state = {
-        month: '',
-        months: 'January',
-        year: '',
-        years: startYears
-    };
-
-      onYearSearch(string, cached){
-        this.setState({agencyName: string});
-        console.log(string, cached);
-      }
-    
-      componentDidMount () {
-        this.mounted = true;
-        var _this = this;
-        this.serverRequest  = APIRequester.getAgencyNamesList()
-          .then(function(result) {    
-            _this.setState({
-                agencyNames: result
-            });
-          })
-        ;
-      }
-    
-      handleOnSelect(item){
-        // the item selected
-        console.log(item);
-      }
-     
-      handleOnFocus(){
-        console.log("Focused");
-      }*/
 
     searchFromDate() {
-        return (
-        <DatePicker selected={this.state.startDate} onChange={date => this.setState({startDate: date})} />
-        );
+      return (
+      <DatePicker selected={this.state.startDate} onChange={date => this.setState({startDate: date})} />
+      );
     } 
 
-    searchToDate() {
-        return (
-        <DatePicker selected={this.state.endDate} onChange={date => this.setState({endDate: date})} />
-        );
-    } 
-    
+    handleChange = date => {
+      date.format();
+    };
 
     render() {
-        //alert(String(this.searchToDate()));
+        let date= parseInt(this.state.startDate.getMonth()+1) + "/" + this.state.startDate.getDate() + "/" +this.state.startDate.getFullYear();
         return (
         <div>
-            <h4>min. date:</h4>
+            <h4 id={date} class="datePicker">Select a date:</h4>
             {this.searchFromDate()}
-            <h4>max date:</h4>
-            {this.searchToDate()}
             <h2>Debt page... in progress</h2>
             <BarChart width={730} height={250} data={data}>
                 <CartesianGrid strokeDasharray="3 3" />
--- src/debtApiRequester.js
+++ src/debtApiRequester.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const baseURL = 'https://transparency.treasury.gov/services/api/fiscal_service/';
-const debtToPenny = baseURL + 'v1/accounting/od/debt_to_penny';
+const debtToPenny = baseURL + 'v2/accounting/od/debt_to_penny';
 
 // Helper function to run a get http request with
 // no explicit parameters.
@@ -14,19 +14,22 @@ async function fetchPage(url, options={}){
     return data;
 }
 
-// Returns a list of dates by month and year.  Preforms API request
-// to get a list of all possible agencies to pick from.
+// Returns a list of date, intragovernmental holdings, public debt outstanding,
+// and outstanding public debt. 
 class Debt {
-    constructor(month, year) {
-      this.month = month;
-      this.year = year;
+    constructor(date, govHolds, public, outstanding) {
+      this.date = date;
+      this.govHolds = govHolds;
+      this.public = public;
+      this.outstanding = outstanding;
     }
 }
 
 async function getDebt(){
     const response = await fetchPage(debtToPenny);
     const result = response.results.map(ele => {
-        return new Debt(ele["reporting_calendar_month"], ele["reporting_calendar_year"])
+        return new Debt(ele["record_date"], ele["intragov_hold_amt"], ele["debt_held_public_amt"], 
+        ele["tot_pub_debt_out_amt"])
     });
     return result;
 }
