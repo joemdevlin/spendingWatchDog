3439f0c17156904bf5528d7568db5e62f391b6f1
tritam <tristantam@gmail.com>
2020-11-28 16:59:59 -0800

    completed homepage with info about each page and clickable divs

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import stateImg from './img/state.png';
+import historicalImg from './img/historical.png';
+import agencyImg from './img/agency.png';
+import debtImg from './img/debt.png';
 
 class HomeView extends Component {
   // Wrapper for about section
@@ -15,16 +19,19 @@ class HomeView extends Component {
           </div>
   }
 
-  // Wrapper for state funding div
+  // Wrapper for state funding info div
   stateWrapper() {
     return <div className="stateWrap col-lg-3">
-      <button type="button">
+      <button type="button" onClick={(e) => {
+          e.preventDefault();
+          window.location.href='#/states';
+          }}>
         <div className="homeDiv">
           <div className="homeTitle">
             <a className="homeTitleText" href="#/states">State Funding</a>
           </div>
-          <div>
-            <img className="stateImg" src={'./img/state.png'} alt="State Graph Icon"></img>
+          <div className="homeImg">
+            <img className="stateImg" src={stateImg} alt="State Graph Icon"></img>
           </div>
           <div className="homeText">
             <p>This page will display total funding by state. All fifty states are listed onto one single circle
@@ -36,16 +43,19 @@ class HomeView extends Component {
     </div>
   }
 
-  // Wrapper for historical funding div
+  // Wrapper for historical funding info div
   historicalWrapper() {
     return <div className="historicalWrap col-lg-3">
-      <button type="button">
+      <button type="button" onClick={(e) => {
+          e.preventDefault();
+          window.location.href='#/historical';
+          }}>
         <div className="homeDiv">
           <div className="homeTitle">
             <a className="homeTitleText" href="#/states">Historical Funding</a>
           </div>
-          <div>
-            <img className="historicalImg" src={require('./img/historical.png')} alt="Historical Graph Icon"></img>
+          <div className="homeImg">
+            <img className="historicalImg" src={historicalImg} alt="Historical Graph Icon"></img>
           </div>
           <div className="homeText">
             <p>An agency name could be searched to display the historical funding onto a line graph. 
@@ -58,16 +68,19 @@ class HomeView extends Component {
     </div>
   }
 
-  // Wrapper for agency funding div
+  // Wrapper for agency funding info div
   agencyWrapper() {
     return <div className="agencyWrap col-lg-3">
-      <button type="button">
+      <button type="button" onClick={(e) => {
+          e.preventDefault();
+          window.location.href='#/breakdown';
+          }}>
         <div className="homeDiv">
           <div className="homeTitle">
             <a className="homeTitleText" href="#/states">Agency Breakdown</a>
           </div>
-          <div>
-            <img className="agencyImg" src={require('./img/agency.jpeg')} alt="Agency Graph Icon"></img>
+          <div className="homeImg">
+            <img className="agencyImg" src={agencyImg} alt="Agency Graph Icon"></img>
           </div>
           <div className="homeText">
             <p>This page displays the current agency funding based on the searched agency.
@@ -80,16 +93,19 @@ class HomeView extends Component {
     </div>
   }
 
-  // Wrapper for debt div
+  // Wrapper for debt info div
   debtWrapper() {
     return <div className="debtWrap col-lg-3">
-      <button type="button">
+      <button type="button" onClick={(e) => {
+          e.preventDefault();
+          window.location.href='#/debt';
+          }}>
         <div className="homeDiv">
           <div className="homeTitle">
             <a className="homeTitleText" href="#/states">Overall Debt</a>
           </div>
-          <div>
-            <img className="debtImg" src={require('./img/debt.png')} alt="Debt Graph Icon"></img>
+          <div className="homeImg">
+            <img className="debtImg" src={debtImg} alt="Debt Graph Icon"></img>
           </div>
           <div className="homeText">
             <p>The overall debt of the United States can be seen in a bar graph. 
